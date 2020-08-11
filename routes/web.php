aea54fc10c2f2c13eca5b1064fd60f82003ec200
shenaach <shenachairani@gmail.com>
2020-08-11 09:46:52 +0700

    Kuis kelar

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "<img src=" . asset('images/erd.png') . ">";
 });
 
-Route::get('/items/create', 'ItemController@create'); // menampilkan halaman form
-Route::post('/items', 'ItemController@store'); // menyimpan data
-Route::get('/items', 'ItemController@index'); // menampilkan semua
-Route::get('/items/{id}', 'ItemController@show'); // menampilkan detail item dengan id 
-Route::get('/items/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
-Route::put('/items/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
-Route::delete('/items/{id}', 'ItemController@destroy'); // menghapus data dengan id
+Route::get('/proyek', 'ProyekController@index');
+Route::get('/proyek/create', 'ProyekController@create');
+Route::post('/proyek', 'ProyekController@store');
+Route::get('/proyek/{id}/daftarkan-staff', 'ProyekController@show');
+Route::post('/proyek/{id}/daftarkan-staff', 'ProyekController@inputstaff');
+Route::get('/proyek/{id}/edit', 'ProyekController@edit');
+Route::put('/proyek/{id}', 'ProyekController@update');
+Route::delete('/proyek/{id}', 'ProyekController@destroy');
+
+Route::get('/karyawan', 'KaryawanController@index');
+Route::get('/karyawan/{id}/daftar-proyek', 'KaryawanController@detail');
